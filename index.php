90e1b5ff88bfaf53730338737d963254d0768c47
Xuya <edgarxuya37@gmail.com>
2026-03-17 18:35:37 +0000

    Enviando archivos al repositorio

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio PHP</title>
+</head>
+<body>
+    <style>
+        body {
+            background-color: #03197a;
+            text-align: center;
+            color: white;
+        }
+    </style>
+</body>
+</html>
+
+<?php
+echo "<h1>Hola Mundo, soy Edgar Xuya</h1> <br>";
+echo '<img src="img.png" alt="Imagen de php" width="350px" height="350px">';
+?>
